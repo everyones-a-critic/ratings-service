027e1f452c4854e7e311532601ea53880a50b7d1
Daniel Long <24.daniel.long@gmail.com>
2022-10-04 16:53:48 -0500

    Adds an output for the load balancer url

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "mongo_uri" {
   description = "The url to use to connect to the mongo cluster"
   value       = mongodbatlas_serverless_instance.main.connection_strings_standard_srv
 }
+
+output "service_uri" {
+  description = "The public internet exposed url used to access the ECS service"
+  value       = aws_lb.main.dns_name
+}
