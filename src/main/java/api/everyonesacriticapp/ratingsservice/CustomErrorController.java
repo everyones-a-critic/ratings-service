9a82760d1455238c52631688d28c4768fbefe9f7
Daniel Long <24.daniel.long@gmail.com>
2022-10-05 17:26:19 -0500

    Customize return values for http errors

---
+++ src/main/java/api/everyonesacriticapp/ratingsservice/CustomErrorController.java
@@ -0,0 +1,31 @@
+package api.everyonesacriticapp.ratingsservice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CustomErrorController implements ErrorController {
+
+    @RequestMapping("/error")
+    public Map<String, Object> handleError(HttpServletRequest request) {
+        Map<String, Object> jsonMap = new HashMap<String, Object>();
+
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        
+        jsonMap.put("message", "An error has occurred.");
+        if (status != null) {
+            if (status.toString().equals("404")) {
+                jsonMap.put("message", "The requested resource was not found.");
+            }
+        }
+
+        return jsonMap;
+    }
+}
