a11d8dd106d030c6cc5160ba1de37cb4014ff76d
Daniel Long <24.daniel.long@gmail.com>
2022-10-19 13:35:52 -0500

    Refactors tests

--- src/test/java/api/everyonesacriticapp/ratingsservice/PostRatingTests.java
+++ src/test/java/api/everyonesacriticapp/ratingsservice/PostRatingTests.java
@@ -41,7 +41,7 @@ public class PostRatingTests {
 	public void testCreateRating() throws Exception {
     	Mockito.when(mongoMock.save(ArgumentMatchers.isA(Rating.class))).thenReturn(null);
 		Mockito.when(mongoMock.save(ArgumentMatchers.isA(Rating.class))).thenReturn(null);
-		this.mockMvc.perform(post("/products/12345/ratings/")
+		this.mockMvc.perform(post("/products/633dcc5180a00f92bf00826c/ratings/")
 		.content("{\"rating\": 4.33, \"comments\": \"This is a test\"}")
 		.header("Authorization", "eyJraWQiOiJYYVduT0g2Y0RQbkVTZktpRUdMU29wK3E5V0d3UCs5R3kwTXZMK2owdFwvQT0iLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOiJiZGJlZjY0ZS05MGRhLTRjOWYtYjQ5Ny0zYTI2ZThjZTEwNzMiLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwiaXNzIjoiaHR0cHM6XC9cL2NvZ25pdG8taWRwLnVzLXdlc3QtMS5hbWF6b25hd3MuY29tXC91cy13ZXN0LTFfUjZGSlRRdTFLIiwiY29nbml0bzp1c2VybmFtZSI6ImJkYmVmNjRlLTkwZGEtNGM5Zi1iNDk3LTNhMjZlOGNlMTA3MyIsIm9yaWdpbl9qdGkiOiIxYjg3YWI5ZC04NzUwLTQxNjgtOTBhYS04NDQ0ZDY2NTU0ZjIiLCJhdWQiOiIycGVwNDdyM3FxdmwwNXViODNqdGszcXVrcCIsImV2ZW50X2lkIjoiNGMxYzRkZGEtYTUzYy00ZWI2LTkxODgtMWRlMzIxYTM4NzhmIiwidG9rZW5fdXNlIjoiaWQiLCJhdXRoX3RpbWUiOjE2NjM3MDE5NzYsImV4cCI6MTY2NjAzNTQ2MSwiaWF0IjoxNjY2MDMxODYxLCJqdGkiOiIxOGNiMmEwYi02ZjAyLTQzMzktODYwYi1kYTlmYjEwMWZlMDciLCJlbWFpbCI6IjI0LmRhbmllbC5sb25nQGdtYWlsLmNvbSJ9.dDPx9eHWdT62dFZ-lWOfQ9dl9ib0AA35qGzY3f9Xobw_31GJ8VdOzyJ1tRhFJEeZTWKEgHJsIHWEal9j_JTXndj0mikxk_S_3sZhrp9rSzAaiywx9gnFiObSkqWCwSuuRE4sRhRwTWuP67eTiSTK9Phx4-Z_2114nNGdfEnHdjdMIZ2OnjIYt-rh5ypl4aeRUgW60JnWroUDgR2o-e_y3a4glmKD-AEQ83xpESsNZSxaoWTsaAnUGr2PWHGxa4IaE-p3cXABOZ5e_IbpX60AoQ9ITcqwaP1epLBDClg1KgyUFpbITJltSYgwTYIr74bEq7UruMyG-_tlIHQRf436vA")
 		.contentType(MediaType.APPLICATION_JSON)
@@ -103,7 +103,7 @@ public class PostRatingTests {
 	@Test
 	public void testUnauthorized() throws Exception {
     	Mockito.when(mongoMock.save(ArgumentMatchers.isA(Rating.class))).thenReturn(null);
-		this.mockMvc.perform(post("/products/12345/ratings/")
+		this.mockMvc.perform(post("/products/633dcc5180a00f92bf00826c/ratings/")
 		.content("{\"rating\": 5.33, \"comments\": \"This is a test\"}")
 		.contentType(MediaType.APPLICATION_JSON)
 		).andDo(print())
@@ -113,7 +113,7 @@ public class PostRatingTests {
 	@Test
 	public void testRatingIsRequired() throws Exception {
     	Mockito.when(mongoMock.save(ArgumentMatchers.isA(Rating.class))).thenReturn(null);
-		this.mockMvc.perform(post("/products/12345/ratings/")
+		this.mockMvc.perform(post("/products/633dcc5180a00f92bf00826c/ratings/")
 		.content("")
 		.contentType(MediaType.APPLICATION_JSON)
 		).andDo(print())
